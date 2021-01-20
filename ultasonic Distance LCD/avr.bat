10eaa073863269da6e92ef71f2c716fb06ea413f
Mohamed Ibrahim Elsawy <moibra95@outlook.com>
2021-01-20 21:48:27 +0200

    update files

--- ultasonic Distance LCD/avr.bat
+++
@@ -1,8 +0,0 @@
-SET target="ultasonic Distance LCD"
-SET mcu=at90s2313
-avr-gcc -g -Os -mmcu=%mcu% -c %target%.c -DF_CPU=4000000UL
-avr-gcc -g -mmcu=%mcu% -o %target%.elf %target%.o -DF_CPU=4000000UL
-avr-objdump -h -S %target%.elf > %target%.lst
-avr-gcc -g -mmcu=%mcu% -Wl,-Map,%target%.map -o %target%.elf %target%.o -DF_CPU=4000000UL
-avr-objcopy -j .text -j .data -O ihex %target%.elf %target%.hex
-
